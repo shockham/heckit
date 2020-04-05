4fba2bf70fa28a65329a47341d154cb8631678ff
shockham <samuel.hockham@gmail.com>
2020-04-05 09:39:29 +0100

    Add title bar

--- heckit.rs
+++ heckit.rs
@@ -8,6 +8,68 @@ trait Component {
     fn to_html(&self) -> String;
 }
 
+
+struct Title;
+
+impl Component for Title {
+    fn style() -> String {
+        "
+            svg {
+                height: 90px;
+                display: inline-block;
+            }
+
+            div {
+                float: right;
+            }
+
+            .links a {
+                color: #fff;
+                transition: all 0.5s ease 0s;
+            }
+
+            .links a:hover {
+                color: #eee;
+                opacity: 0.7;
+            }
+        ".to_string()
+    }
+
+    fn to_html(&self) -> String {
+        "
+            <svg
+              xmlns=\"http://www.w3.org/2000/svg\"
+              preserveAspectRatio=\"xMidYMin meet\" viewBox=\"0 0 68 90\"
+            >
+              <g
+                transform=\"translate(-68.297193,-19.514977)\"
+              >
+                <path
+                  style=\"fill:#ffffff;fill-opacity:1;stroke:none;stroke-width:0.26458332px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1\"
+                  d=\"M 81.52636,108.52836 V 84.337891 l -13.229167,-16.252975 2.83482,-20.221729 22.300596,-14.174104 6.425594,2.456844 0.188977,9.827381 -11.906237,12.284226 13.229157,12.473215 -0.11071,25.713844 3.07176,0.05011 0.28955,-25.485234 12.96257,-12.962573 -12.16076,-12.428032 0.13364,-7.216277 27.66239,9.621702 3.0736,21.782466 -9.75534,12.294399 0.26727,26.326046 z\"
+                />
+                <path
+                  style=\"fill:#ffffff;fill-opacity:1;fill-rule:evenodd;stroke:none;stroke-width:0.26499999;stroke-linecap:butt;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1\"
+                  d=\"m 103.82727,56.710551 -0.0438,-4.953431 4.36042,4.979675 z m -6.422818,-3e-6 h 4.810848 v -4.944485 z m -4.81085,1.469983 10.156238,-10.289879 10.0226,10.022607 -9.6217,10.156243 z\"
+                />
+                <path
+                  style=\"fill:#ffffff;fill-opacity:1;stroke:none;stroke-width:0.26499999;stroke-linecap:butt;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1\"
+                  d=\"m 74.550502,19.536919 -3.2295,24.82999 20.977677,-13.607142 40.537941,13.796129 -3.38998,-25.029109 -10.83137,-0.01181 -0.14175,14.174106 -2.25116,-0.0057 1e-5,-14.110157 -12.35055,-0.01958 -0.0449,9.03278 h -2.26786 l -0.0725,-9.03802 -14.084847,-0.02244 -0.07576,9.473867 -2.114303,-0.03543 v -9.449404 z\"
+                />
+              </g>
+            </svg>
+            <div class=\"links\">
+              <a href=\"https://twitter.com/shockham\" target=\"_blank\">tw</a>
+              <a href=\"https://github.com/shockham\" target=\"_blank\">gh</a>
+              <a href=\"https://shockham.bandcamp.com/\" target=\"_blank\">bc</a>
+              <a rel=\"me\" href=\"https://merveilles.town/@shockham\" target=\"_blank\">
+                  fedi
+              </a>
+            </div>
+        ".to_string()
+    }
+}
+
 struct Project {
     title: &'static str,
     desc: &'static str,
@@ -87,6 +149,7 @@ impl Component for App {
 
 
 struct Root {
+    title_html: String,
     body_html: String,
 }
 
@@ -107,20 +170,29 @@ impl Component for Root {
                 box-sizing: border-box;
                 font-family: monospace;
             }
+
+            h1 {
+                display: inline-block;
+                border-bottom: 1px solid #fff;
+                width: 100%;
+                height: 88px;
+                margin: 0;
+            }
         ".to_string()
     }
 
     fn to_html(&self) -> String {
         let global_style = Root::style();
+        let title_style = Title::style();
         let projects_style = Project::style();
         let app_style = App::style();
-        let mut all_style = format!(
-            "{}{}{}",
+        let all_style = format!(
+            "{}{}{}{}",
             global_style,
+            title_style,
             app_style,
             projects_style,
         );
-        all_style.retain(|c| c != ' ');
 
         format!(
             "<!DOCTYPE html>
@@ -132,11 +204,14 @@ impl Component for Root {
                 </style>
             </head>
             <body>
-                <h1>shockham</h1>
+                <h1>
+                    {title}
+                </h1>
                 {body}
             </body>
             </html>\r",
             style=all_style,
+            title=self.title_html,
             body=self.body_html,
         )
     }
@@ -217,11 +292,16 @@ fn main() {
         .map(|proj| proj.to_html())
         .collect::<String>();
 
+    let title_html = Title {}.to_html();
+
     let app = App { projects_html };
 
     let body_html = app.to_html();
 
-    let root = Root { body_html };
+    let root = Root {
+        title_html,
+        body_html
+    };
 
     let res_string = format!(
         "HTTP/1.1 200 OK\r
